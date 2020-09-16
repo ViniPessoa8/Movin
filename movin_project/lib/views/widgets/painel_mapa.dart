dcb0fa51b48f74128a34776feb74101cdba864f3
Vinicius <vini.pessoa7@gmail.com>
2020-09-15 23:07:22 -0400

    feat: estrutura inicial das paginas principais

---
+++ movin_project/lib/views/widgets/painel_mapa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PainelMapa extends StatefulWidget {
+  @override
+  _PainelMapaState createState() => _PainelMapaState();
+}
+
+class _PainelMapaState extends State<PainelMapa> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Mapa'),
+    );
+  }
+}
