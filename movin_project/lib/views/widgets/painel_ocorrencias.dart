a997cd8f436a4aaf105dcf8afbc5837a15ae797e
Vinicius <vini.pessoa7@gmail.com>
2020-09-21 12:16:02 -0400

    feat: painel_ocorrencias

--- movin_project/lib/views/widgets/painel_ocorrencias.dart
+++ movin_project/lib/views/widgets/painel_ocorrencias.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:movin_project/models/ocorrencia.dart';
+import 'package:movin_project/views/widgets/item_ocorrencia.dart';
 
-class PainelOcorrencias extends StatelessWidget {
+class PainelOcorrencias extends StatefulWidget {
+  final List<Ocorrencia> ocorrencias;
+
+  PainelOcorrencias(this.ocorrencias);
+
+  @override
+  _PainelOcorrenciasState createState() => _PainelOcorrenciasState();
+}
+
+class _PainelOcorrenciasState extends State<PainelOcorrencias> {
+  Widget _imprimeOcorrencias() {
+    return ListView.builder(
+      itemCount: widget.ocorrencias.length,
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+      itemBuilder: (context, index) {
+        return ItemOcorrencia(widget.ocorrencias[index]);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Ocorrências'),
+      child: widget.ocorrencias.isEmpty
+          ? Text('Não há ocorrências.')
+          : _imprimeOcorrencias(),
     );
   }
 }
