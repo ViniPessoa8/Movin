9bf9dff07541972883c7c40af6aeca7ada17e530
Vinicius <vini.pessoa7@gmail.com>
2020-09-21 18:42:13 -0400

    feat: cria ocorrencia

--- movin_project/lib/views/widgets/painel_ocorrencias.dart
+++ movin_project/lib/views/widgets/painel_ocorrencias.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movin_project/models/ocorrencia.dart';
 import 'package:movin_project/views/items/item_ocorrencia.dart';
+import 'package:movin_project/views/widgets/painel_cria_ocorrencia.dart';
 
 class PainelOcorrencias extends StatefulWidget {
   final List<Ocorrencia> ocorrencias;
@@ -11,18 +12,43 @@ class PainelOcorrencias extends StatefulWidget {
   _PainelOcorrenciasState createState() => _PainelOcorrenciasState();
 }
 
+void _mostraCriaOcorrencia(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (context) {
+      return PainelCriaOcorrencia();
+    },
+  );
+}
+
 class _PainelOcorrenciasState extends State<PainelOcorrencias> {
   Widget _imprimeOcorrencias() {
-    return SingleChildScrollView(
-      child: Container(
-        height: 800,
-        child: ListView.builder(
-          itemCount: widget.ocorrencias.length,
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-          itemBuilder: (context, index) {
-            return ItemOcorrencia(widget.ocorrencias[index]);
-          },
-        ),
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 600,
+            child: SingleChildScrollView(
+              child: Container(
+                height: 800,
+                child: ListView.builder(
+                  itemCount: widget.ocorrencias.length,
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                  itemBuilder: (context, index) {
+                    return ItemOcorrencia(widget.ocorrencias[index]);
+                  },
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              _mostraCriaOcorrencia(context);
+            },
+          ),
+        ],
       ),
     );
   }
