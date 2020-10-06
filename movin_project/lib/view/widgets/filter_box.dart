024d1571d8cc29517f383c081cdb703555eb297c
Vinicius <vini.pessoa7@gmail.com>
2020-10-05 21:19:31 -0400

    feat: multiselect dependencies + filterbox

---
+++ movin_project/lib/view/widgets/filter_box.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:movin_project/model_view/model_view.dart';
+import 'package:multiselect_formfield/multiselect_formfield.dart';
+
+class FilterBox extends StatelessWidget {
+  final ModelView mv;
+
+  FilterBox(this.mv);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MultiSelectFormField(
+        dataSource: [
+          {'display': 'Meu bairro', 'value': 'Bairro'}
+        ],
+        textField: 'display',
+        valueField: 'value',
+        onSaved: (newValue) {
+          print('Valor: $newValue');
+        },
+      ),
+    );
+  }
+}
