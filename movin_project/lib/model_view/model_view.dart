b33748a6b925135078ee1e42c6d16547076da189
Vinicius <vini.pessoa7@gmail.com>
2020-10-07 19:58:15 -0400

    feat: modelview upload imagem

--- movin_project/lib/model_view/model_view.dart
+++ movin_project/lib/model_view/model_view.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:geopoint/geopoint.dart' as gp;
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:location/location.dart';
 import 'package:mapbox_gl/mapbox_gl.dart' as mb;
@@ -90,6 +93,9 @@ class ModelView extends Model {
 
   // Ocorrencia
 
+  List<File> imagens = [];
+  ImagePicker _imgPicker = new ImagePicker();
+
   void addOcorrencia(Ocorrencia ocorrencia) async {
     bool resp = await _fc.addOcorrencia(
       descricao: ocorrencia.descricao,
@@ -104,14 +110,6 @@ class ModelView extends Model {
     } else {
       print('Erro na criação da ocorrência.');
     }
-  }
-
-  // Endereço
-
-  Future<Address> getEnderecoBD(double latitude, double longitude) async {
-    Address result;
-    result = await _fc.fetchEndereco(latitude, longitude);
-    return result;
   }
 
   String formatEndereco(Address endereco) {
@@ -130,6 +128,40 @@ class ModelView extends Model {
   String formatData(DateTime data) {
     final DateFormat formatadorData = DateFormat('dd/MM/yyyy\nHH:mm');
     return formatadorData.format(data);
+  }
+
+  void uploadImagem(File imagem) {
+    _fc.uploadFile(imagem);
+  }
+
+  imgCamera() async {
+    PickedFile img = await _imgPicker.getImage(
+      source: ImageSource.camera,
+      imageQuality: 80,
+    );
+    print('Imagem selecionada: ${img.path}');
+
+    imagens.add(File(img.path));
+    notifyListeners();
+  }
+
+  imgGaleria() async {
+    PickedFile img = await _imgPicker.getImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+    );
+    print('Imagem selecionada: ${img.path}');
+
+    imagens.add(File(img.path));
+    notifyListeners();
+  }
+
+  // Endereço
+
+  Future<Address> getEnderecoBD(double latitude, double longitude) async {
+    Address result;
+    result = await _fc.fetchEndereco(latitude, longitude);
+    return result;
   }
 
   // void logaUsuario(BuildContext context) {
