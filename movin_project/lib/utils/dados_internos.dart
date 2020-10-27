ff37ce3f99a998ab2505e344a1a2dcc530d0f103
Vinicius <vini.pessoa7@gmail.com>
2020-10-27 16:27:28 -0400

    fix: deleta dados inuteis

--- movin_project/lib/utils/dados_internos.dart
+++ movin_project/lib/utils/dados_internos.dart
@@ -12,49 +12,4 @@ class DadosInternos {
     'Iluminação',
     'Obra',
   ];
-
-  static final List<Ocorrencia> OCORRENCIAS_EXEMPLO = [
-    Ocorrencia(
-      idOcorrencia: 0,
-      idAutor: 0,
-      descricao: 'Assalto à mão armada',
-      categoria: 'Assaltos e Furtos',
-      data: DateTime.now(),
-      // local: null,
-    ),
-    Ocorrencia(
-      idOcorrencia: 1,
-      idAutor: 0,
-      descricao: 'Buraco na calçada',
-      categoria: 'Infraestrutura',
-      data: DateTime.now(),
-      // local: null,
-    ),
-    Ocorrencia(
-      idOcorrencia: 2,
-      idAutor: 1,
-      descricao: 'Trânsito intenso sentido bairro.',
-      categoria: 'Trânsito',
-      data: DateTime.now(),
-      // local: null,
-    ),
-    // Ocorrencia(
-    //   idOcorrencia: 3,
-    //   idAutor: 2,
-    //   titulo: 'TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE',
-    //   descricao: 'Testeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.',
-    //   categoria: 'testeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-    //   data: DateTime.now(),
-    //   // local: null,
-    // ),
-    // Ocorrencia(
-    //   idOcorrencia: 4,
-    //   idAutor: 2,
-    //   titulo: 'Jogador de Free-Fire',
-    //   descricao: 'Pessoa jogando Free-Fire publicamente.',
-    //   categoria: 'Desrespeito Geral',
-    //   data: DateTime.now(),
-    //   // local: null,
-    // ),
-  ];
 }
